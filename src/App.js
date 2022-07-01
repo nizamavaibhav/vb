d2433f6920e9bc66c7cc4b245b41331612c200ff
nizamavaibhav <nizamavaibhav@gmail.com>
2022-07-01 11:52:02 +0530

    first-commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from "./components/Navbar";
+import Card from "./components/Card"
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <Card></Card>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+function Card() {
+return(
+<div>
+    <h1>Vaibhav Nizama</h1>
+<p>This my original name</p>
+</div>
+);
+}
+
+export default Card;
+   
